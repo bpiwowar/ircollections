0e91b3ea4daf6e5a1d393e5b01f06bdb37e58365
Benjamin Piwowarski <benjamin@bpiwowar.net>
2015-05-20 17:54:10 +0200

    Evaluate multiple runs at once

--- etc/irc.js
+++ etc/irc.js
@@ -22,9 +22,9 @@ var module_irc = {
 	name: "Information Retrieval Collections",
 	url: "http://ircollections.sourceforge.net",
 	id: qname(irc, "main"),
-	
+
 	description: <p>
-		This contains the task associated with the IR collections project, which aims at 
+		This contains the task associated with the IR collections project, which aims at
 		providing an unique interface for running ad-hoc IR experiments.
 	</p>
 };
@@ -34,31 +34,31 @@ module = xpm.add_module(module_irc);
 
 // --- Get a task XML definition
 
-function get_task(p) { 
+function get_task(p) {
 	logger.info("IR task is [%s], restriction [%s]", $(p.id), $(p.restrict));
 
 	args= [path(irc_bin), $(p.command), "--json", "--engine", $(p.engine)];
-	if ($(p.restrict)) 
+	if ($(p.restrict))
 		args = args.concat("--restrict", $(p.restrict));
-					
-	// Run ircollections 
+
+	// Run ircollections
 	var command = args.concat($(p.id));
-	
+
 	logger.debug("arguments are %s; output=", command.toString());
 
 	output = xpm.evaluate(command);
 	// Get the output
-		
+
     logger.debug("Output is: %s", output);
-	return JSON.parse(output.trim());	
+	return JSON.parse(output.trim());
 }
 
 tasks.add("irc:get-task", {
     module: module_irc.id,
-    
+
 	// The description of this experiment
 	description: "<p>Get the full description of an IR collection, including the list of files, the topics, and the qrels</p>",
-    
+
     inputs: {
         command: { value: "xp:string", "default": "prepare"},
         id: { value: "xp:string", help: "The name of the IR task (e.g. trec.7/adhoc)" },
@@ -67,7 +67,7 @@ tasks.add("irc:get-task", {
     },
 
     output: qname(irc, "task"),
-    
+
     run: get_task
 });
 
@@ -95,8 +95,7 @@ var task_evaluate = {
 
 	run: function(p) {
 	   var outputPath = p.out ? $(p.out) : $(p.run.path).add_extension(".eval");
-       logger.debug("Output path: %s", outputPath);
-       logger.info("Run: Resource %s", p.run.$resource)
+       logger.debug("Output path: %s [%s]", outputPath, $$(p.run));
 		var command = [path(irc_bin), "evaluate", "--json"];
         if ($(p.details)) {
             command.push("--details");
@@ -104,21 +103,21 @@ var task_evaluate = {
         command.push($(p.run.path), new ParameterFile("qrels", p.qrels.toSource()));
 
 		var rsrc = xpm.command_line_job(outputPath, command,
-			{ 
+			{
 				lock: [[ $$(p.run), "READ_ACCESS" ]],
 				stdout: outputPath,
-			} 
+			}
 		);
 
 		// Run the evaluation
 		var r = {
-            $type: "irc:run",
+            $type: "irc:evaluation",
             $resource: rsrc,
 		    path: outputPath,
             run: p.run,
             qrels: p.qrels
 		}
-		
+
 		return r;
 	}
 };
@@ -126,6 +125,53 @@ var task_evaluate = {
 
 xpm.add_task_factory(task_evaluate);
 
+
+
+/**
+ * Evaluate a set of runs
+ */
+var task_evaluate = {
+    id: qname(irc, "evaluate-runs"),
+
+    module: module_irc.id,
+
+    description: "<p>Evaluate a series of runs</p>",
+
+    inputs: {
+        "runs": { array: "irc:run", help: "The runs to evaluate" },
+        "qrels": { json: "irc:qrels", help: "The relevance assessments" },
+        "details": { json: "xp:boolean", help: "Whether details results should be returned (query level)" },
+        "basedir": { json: "xp:path", optional: true, help: "The directory in which the task should be created"},
+        "$extension": { json: "xp:string", default: ".eval" }
+    },
+
+    run: function(p) {
+        var taskpath = $(p.basedir) ? this.unique_file($(p.basedir), "irc.evaluate", p) :  this.unique_file("irc.evaluate", p);
+        var command = [path(irc_bin), "evaluate-runs", "--json"];
+        if ($(p.details)) {
+            command.push("--details");
+        }
+        command.push(p.runs.as_parameter_file("runs"), new ParameterFile("qrels", p.qrels.toSource()));
+        var rsrc = xpm.command_line_job(taskpath, command, { lock: get_locks("READ", p) });
+
+        // Run the evaluation
+        var result = [];
+        for(var i = 0; i < p.runs.length; i++) {
+            result.push({
+                $type: "irc:evaluation",
+                $resource: rsrc,
+                path: $(p.runs[i].path).add_extension($(p.$extension)),
+                run: p.runs[i],
+                qrels: p.qrels
+            });
+        }
+
+        return result;
+    }
+};
+
+
+xpm.add_task_factory(task_evaluate);
 
 /**
  * Evaluate a run : hypotheses about evaluation are the same than
